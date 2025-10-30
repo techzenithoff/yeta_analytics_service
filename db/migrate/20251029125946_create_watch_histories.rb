1937b2ed960c827cc9a00e8ccc0cdf6a7af65db8
thkernel <salut.amos@gmail.com>
2025-10-30 15:14:48 +0000

    update

--- db/migrate/20251029125946_create_watch_histories.rb
+++ db/migrate/20251029125946_create_watch_histories.rb
@@ -2,8 +2,8 @@ class CreateWatchHistories < ActiveRecord::Migration[6.1]
   def change
     create_table :watch_histories do |t|
       t.string :uid
-      t.references :watchable, null: false, foreign_key: true
-      t.references :account, null: false, foreign_key: true
+      t.references :watchable, polymorphic: true, index: true
+      t.references :account, index: true #null: false, foreign_key: true
       t.datetime :started_at
       t.datetime :last_watched_at
       t.integer :position_seconds
